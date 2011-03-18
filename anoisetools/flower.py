2bed261d95688f5ad554e4d88547aed17390a645
Connor McCoy <cmccoy@fhcrc.org>
2011-03-18 16:08:44 -0700

    flower.py change rounding method

--- anoisetools/flower.py
+++ anoisetools/flower.py
@@ -21,7 +21,8 @@ def flow_to_seq(flowgram, flow_order=FLOW_ORDER):
     result = []
     for read_value, base in zip(flowgram, bases):
         # Round to the nearest value
-        signal = int(round(read_value, 0))
+        signal = int(read_value + 0.5)
+        #signal = int(round(read_value, 0))
         result.extend([base] * signal)
 
     return ''.join(result)
