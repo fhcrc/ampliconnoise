e4016673cad72e05f6be10a8e0c9ed528aba5c01
Connor McCoy <cmccoy@fhcrc.org>
2011-03-18 09:29:13 -0700

    Add flower reader, some tests

--- anoisetools/flower.py
+++ anoisetools/flower.py
@@ -3,6 +3,7 @@ Tools for working with raw flower output.
 """
 
 import itertools
+import re
 
 # Sequence in which nucleotides are flowed:
 FLOW_ORDER = 'TACG'
@@ -26,3 +27,60 @@ def flow_to_seq(flowgram, flow_order=FLOW_ORDER):
     return ''.join(result)
 
 
+class FlowerRecord(object):
+    def __init__(self, identifier):
+        self.identifier = identifier
+        self.info = None
+        self.clip = None
+        self.flows = None
+        self.index = None
+        self.bases = None
+        self.quals = None
+
+
+#>FTWCYXX01BTPDQ
+#  Info:   2009-04-08 11:31:35 R1 (631,1068)
+#  Clip:   5 282
+#  Flows:  1.04 0.01 1.02 0.07 0.05 0.97 0.06 2.00 0.96 1.09 0.98 0.08 0.02 1.18   Index:  1 3 6 8 8 9 10 11 14 16 18 18 18 18 18 21 21 23 23 23 23 25 27 27 27 3  Bases:  tcagGTACAGAAAAATTCCCCTCCCAATTAAAACTATGTGATGTGATTTCTATGTCCCCTCCTGAGGGTT  Quals:  37 37 37 37 37 37 37 37 38 34 20 20 20 20 15 34 34 37 37 37 37 38 34 3>FTWCYXX01B0TTO
+#  Info:   2009-04-08 11:31:35 R1 (712,1642)
+#  Clip:   5 278
+#  Flows:  1.04 0.04 1.01 0.09 0.06 0.99 0.09 1.97 1.02 1.06 0.98 0.07 0.01 1.20
+#  Index:  1 3 6 8 8 9 10 11 14 16 18 18 18 18 18 21 21 23 25 27 27 29 31 33 35 3  Bases:  tcagGTACAGAAAAATTCTCCTCTCAATTAAAACTATGTGATGTGATTTCTATGTCCCCTCCTGAGGGTT  Quals:  37 37 37 37 37 37 37 37 38 38 28 28 28 28 28 38 38 37 37 37 37 37 40
+#
+
+_HEADER_REGEXP = re.compile(r'^\s*>(.*)')
+def _is_header(line):
+    return line and _HEADER_REGEXP.match(line)
+
+_LINE_REGEXP = re.compile('^\s*(\w+):\s+(.*)$')
+
+def read_flower(iterable):
+    header = next(iterable)
+    if not _is_header(header):
+        raise ValueError("Invalid record identifier: {}".format(header))
+
+    record = FlowerRecord(header[1:])
+
+    while True:
+        try:
+            line = next(iterable)
+        except StopIteration:
+            yield record
+            raise StopIteration()
+        if _is_header(line):
+            yield record
+            record = FlowerRecord(_HEADER_REGEXP.match(line).group(1))
+        m = _LINE_REGEXP.match(line)
+        if not m:
+            # Ignore blank lines for now
+            continue
+        key, value = m.groups()
+
+        if key in ('Info', 'Bases'):
+            setattr(record, key.lower(), value)
+        elif key in ('Clip', 'Index', 'Quals'):
+            setattr(record, key.lower(), map(int, value.split()))
+        elif key == 'Flows':
+            record.flows = map(float, value.split())
+        else:
+            raise ValueError("Unknown key: {}".format(key))
--- anoisetools/test/test_flower.py
+++ anoisetools/test/test_flower.py
@@ -13,3 +13,66 @@ class TestFlowToSeq(unittest.TestCase):
         actual = flower.flow_to_seq(self.test_flow)
         self.assertEqual(self.test_expected, actual)
 
+class TestFlowerReader(unittest.TestCase):
+
+    def setUp(self):
+        self.test_iter = """>FTWCYXX01BTPDQ
+  Info:   2009-04-08 11:31:35 R1 (631,1068)
+  Clip:   5 282
+  Flows:  1.04 0.01 1.02 0.07 0.05 0.97 0.06 2.00 0.96 1.09 0.98 0.08 0.02 1.18
+  Index:  1 3 6 8 8 9 10 11 14 16 18 18 18 18 18 21 21 23 23 23 23 25 27 27 27 3
+  Bases:  tcagGTACAGAAAAATTCCCCTCCCAATTAAAACTATGTGATGTGATTTCTATGTCCCCTCCTGAGGGTT
+  Quals:  37 37 37 37 37 37 37 37 38 34 20 20 20 20 15 34 34 37 37 37 37 38 34 3
+  >FTWCYXX01B0TTO
+  Info:   2009-04-08 11:31:35 R1 (712,1642)
+  Clip:   5 278
+  Flows:  1.04 0.04 1.01 0.09 0.06 0.99 0.09 1.97 1.02 1.06 0.98 0.07 0.01 1.20
+  Index:  1 3 6 8 8 9 10 11 14 16 18 18 18 18 18 21 21 23 25 27 27 29 31 33 35 3
+  Bases:  tcagGTACAGAAAAATTCTCCTCTCAATTAAAACTATGTGATGTGATTTCTATGTCCCCTCCTGAGGGTT
+  Quals:  37 37 37 37 37 37 37 37 38 38 28 28 28 28 28 38 38 37 37 37 37 37 40""".splitlines()
+
+        self.reader = flower.read_flower(iter(self.test_iter))
+
+    def tearDown(self):
+        pass
+
+    def test_read_1(self):
+        record = self.reader.next()
+        self.assertEquals("2009-04-08 11:31:35 R1 (631,1068)", record.info)
+        self.assertEquals([5, 282], record.clip)
+
+        self.assertEquals(14, len(record.flows))
+
+    def test_read_clip(self):
+        record = self.reader.next()
+        self.assertEquals([5, 282], record.clip)
+        record = self.reader.next()
+        self.assertEquals([5, 278], record.clip)
+
+    def test_read_flows(self):
+        record = self.reader.next()
+        self.assertEquals(14, len(record.flows))
+        self.assertEquals(1.04, record.flows[0])
+        self.assertEquals(1.18, record.flows[13])
+
+    def test_read_identifier(self):
+        record = self.reader.next()
+        self.assertEquals("FTWCYXX01BTPDQ", record.identifier)
+
+    def test_read_bases(self):
+        record = self.reader.next()
+        self.assertEquals('tcagGTACAGAAAAATTCCCCTCCCAATTAAAACTATGTGATGTGATTTCTATGTCCCCTCCTGAGGGTT',
+                          record.bases)
+        record = self.reader.next()
+        self.assertEquals('tcagGTACAGAAAAATTCTCCTCTCAATTAAAACTATGTGATGTGATTTCTATGTCCCCTCCTGAGGGTT',
+                          record.bases)
+
+    def test_read_all(self):
+        """
+        Make sure all the records are read
+        """
+        records = list(self.reader)
+        self.assertEquals(2, len(records))
+        self.assertEquals(['FTWCYXX01BTPDQ', 'FTWCYXX01B0TTO'],
+                          [record.identifier for record in records])
+
