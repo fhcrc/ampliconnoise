90fb2140d75dc0775414d9647586f1c9c8fd52e3
Connor McCoy <cmccoy@fhcrc.org>
2011-03-23 15:29:11 -0700

    Update README, remove cruft
    * Trimmed some commented lines
    * Added a quick blurb about the subcommands to the README

--- ampliconnoise/scripts/__init__.py
+++ ampliconnoise/scripts/__init__.py
@@ -7,13 +7,12 @@ import split
 import truncate
 
 SUBCOMMANDS = ('clean', 'split', 'truncate')
-#PARSER_BUILDERS = {
-#    'clean': (clean.build_parser, clean.main),
-#    'split': (split.build_parser, split.main),
-#    'truncate': (truncate.build_parser, truncate.main),
-#}
+
 
 def main(args=sys.argv[1:]):
+    """
+    Parses arguments, passes execution to the appropriate subcommand.
+    """
     parser = argparse.ArgumentParser(description="""Prepare data for use with
 AmpiclonNoise""")
     subparsers = parser.add_subparsers(title='Commands')
@@ -23,12 +22,6 @@ AmpiclonNoise""")
         p_module = importlib.import_module('.' + command_name, __package__)
         p_module.build_parser(p)
         p.set_defaults(func=p_module.main)
-
-#    for command_name, (builder, func) in PARSER_BUILDERS.items():
-#
-#        p = subparsers.add_parser(command_name)
-#        builder(p)
-#        p.set_default(func=func)
 
     parsed_args = parser.parse_args(args)
     parsed_args.func(parsed_args)
